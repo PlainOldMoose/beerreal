4e347562bb06ed50924f2e76b1f10c1ea77d0caa
Luque <luque@vandermerwe.uk>
2026-01-06 17:41:24 +0000

    Implemented User Repository

--- src/main/java/com/beerreal/beerreal/repository/UserRepository.java
+++ src/main/java/com/beerreal/beerreal/repository/UserRepository.java
@@ -1,4 +1,14 @@
 package com.beerreal.beerreal.repository;
 
-public class UserRepository {
+import com.beerreal.beerreal.model.User;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository {
+    Optional<User> findByUserName(String username);
+    Optional<User> findByEmail(String email);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
 }
