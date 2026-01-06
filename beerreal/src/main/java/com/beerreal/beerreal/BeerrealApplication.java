74960483f77629b4384f899c531b3bd132beb7a0
Luque <luque@vandermerwe.uk>
2026-01-06 16:53:56 +0000

    Initial Commit

---
+++ beerreal/src/main/java/com/beerreal/beerreal/BeerrealApplication.java
@@ -0,0 +1,13 @@
+package com.beerreal.beerreal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerrealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerrealApplication.class, args);
+	}
+
+}
---
+++ beerreal/src/test/java/com/beerreal/beerreal/BeerrealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beerreal.beerreal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerrealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
