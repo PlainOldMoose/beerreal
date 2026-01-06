6d300f6d170e94a188ccc604f37640e206fff7f2
Luque <luque@vandermerwe.uk>
2026-01-06 17:25:49 +0000

    Implemented User class

--- src/main/java/com/beerreal/beerreal/model/User.java
+++ src/main/java/com/beerreal/beerreal/model/User.java
@@ -1,4 +1,40 @@
 package com.beerreal.beerreal.model;
 
+import jakarta.persistence.*;
+import jdk.jfr.Enabled;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(name = "created_at")
+    private LocalDateTime created_at;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<BeerEntry> beerEntries = new ArrayList<>();
+
+    @PrePersist
+    protected void onCreate() {
+        created_at = LocalDateTime.now();
+    }
 }
