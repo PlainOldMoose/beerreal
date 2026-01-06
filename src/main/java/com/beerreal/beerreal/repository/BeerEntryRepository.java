cd0b4f81a7e3bdb2f51e966dd4a386a7b8ca3609
Luque <luque@vandermerwe.uk>
2026-01-06 17:37:50 +0000

    Implemented BeerEntryRepository

--- src/main/java/com/beerreal/beerreal/repository/BeerEntryRepository.java
+++ src/main/java/com/beerreal/beerreal/repository/BeerEntryRepository.java
@@ -1,4 +1,20 @@
 package com.beerreal.beerreal.repository;
 
-public class BeerEntryRepository {
+import com.beerreal.beerreal.model.BeerEntry;
+import com.beerreal.beerreal.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BeerEntryRepository extends JpaRepository<BeerEntry, Integer> {
+    List<BeerEntry> findByUserOrderByCreatedAtDesc(User user);
+    List<BeerEntry> findByYearOrderByCReatedAtDeesc(Integer year);
+
+    @Query("SELECT e.user, COUNT(e) as entryCount FROM BeerEntry e WHERE e.year = :year GROUP BY e.user ORDER BY entryCount DESC")
+    List<Object[]> findLeeaderboardByYear(Integer year);
+
+    long countByUserAndYear(User user, Integer year);
 }
